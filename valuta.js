c1d638f9368d2daae2402baad999f6eac1cf2768
Ivan <iadomocmat@gmail.com>
2026-02-27 15:00:38 +0100

    Add files via upload

--- valuta.js
+++ valuta.js
@@ -1,18 +1,45 @@
 const apiKey = '13d7882aeb54787e6d8daae8';
+
+//Knapp for konverter
 const knapp = document.getElementById("konverter");
-knapp.addEventListener('click',function(){
-	console.log("HEI");
+
+//input felt
+const amountInput = document.getElementById("amount");
+
+//begge valuta menyer
+const fromCurrency = document.getElementById("from");
+const toCurrency = document.getElementById("to");
+
+// swap button
+const swapBtn = document.getElementById("swapBtn");
+
+
+//resultat
+const result = document.getElementById("result");
+
+// aamountInput.value ganger med toCurrecny.value som ligger i data.conversion_rates
+function logExchangeRateInfo(data) {
+	result.textContent = ( amountInput.value * data.conversion_rates[toCurrency.value]);
+}
+
+//saw button function
+swapBtn.addEventListener('click', function(){
+	let temp = fromCurrency.value
+	fromCurrency.value = toCurrency.value
+	toCurrency.value = temp
 })
 
 
+//Functions
+knapp.addEventListener('click',function(){
+	console.log(amountInput.value);
+	console.log(fromCurrency.value);
+	console.log(toCurrency.value);
 
 
-function logExchangeRateInfo(data) {
-	document.getElementById("tekst").textContent = ('1 ' +data.base_code+' to NOK '+ data.conversion_rates.NOK);
-}
 
-
-fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`)
+// fromCurrency.value henter currency fra api
+	fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`)
 	.then(response => response.json())
 	.then(data => {
 		logExchangeRateInfo(data);
@@ -20,6 +47,16 @@ fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`)
 	.catch(error => {
 		console.error('Error:', error);
 	});
+
+})
+
+
+
+
+
+
+
+
 
 
 
